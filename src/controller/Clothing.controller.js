f0c4b6193bccc119e24fdea84ff95388470a934c
Tyler Huang <huangtyler20@gmail.com>
2023-10-03 19:30:35 -0400

    created clothing feed

--- src/controller/Clothing.controller.js
+++ src/controller/Clothing.controller.js
@@ -2,5 +2,18 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Clothing = mongoose.model('Clothing');
+const Outfit = mongoose.model('Outfit');
+
+router.get('/clothing-feed', (req, res) => {
+    Outfit.find()
+    .sort({createdAt: -1})
+    .then((result) => {
+        console.debug("Retrieved all Clothing in timeline view");
+        res.send({result: result}).status(200);
+    }).catch((error) => {
+        console.error("Unable to retrieve all clothing objects", error);
+        res.send({message: "failure", reason: error}).status(500);
+    });
+});
 
 module.exports = router;
