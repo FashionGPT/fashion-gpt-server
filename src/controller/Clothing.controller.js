4c29100f06553763619c6ae0665210f68e992c06
ameenagbaria <144632236+ameenagbaria@users.noreply.github.com>
2023-10-17 19:30:17 -0400

    Favorite clothing 2 (#28)
    Co-authored-by: Ameen Agbaria <ameenagbaria@lawn-128-61-73-31.lawn.gatech.edu>
    Co-authored-by: Ameen Agbaria <ameenagbaria@lawn-128-61-71-50.lawn.gatech.edu>
    Co-authored-by: Deepak Ramalingam <rdeepak2002@gmail.com>

--- src/controller/Clothing.controller.js
+++ src/controller/Clothing.controller.js
@@ -16,4 +16,17 @@ router.get('/clothing-feed', (req, res) => {
     });
 });
 
+router.post('/get-favorites', (req, res) => {
+    Outfit.find({
+        isFavorite : true,
+        userID: new mongoose.Types.ObjectId(req.body.userID)
+    }).then((result) => {
+        console.debug("Retrieved all favorite outfits from the user");
+        res.send({result: result}).status(200);
+    }).catch((error) => {
+        console.error("Unable to retrieve all favorite outfits from the user", error);
+        res.send({message: "failure", reason: error}).status(500);
+    });
+});
+
 module.exports = router;
