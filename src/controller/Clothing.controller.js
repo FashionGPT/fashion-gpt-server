f7a28c42171760c23c002d600e17bb3eb2c7561d
Ameen Agbaria <ameenagbaria@lawn-128-61-73-31.lawn.gatech.edu>
2023-10-16 21:16:41 -0400

    made changes for favorite

--- src/controller/Clothing.controller.js
+++ src/controller/Clothing.controller.js
@@ -2,5 +2,20 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Clothing = mongoose.model('Clothing');
+const Outfit = mongoose.model('Outfit');
+
+
+router.post('/get-favorites', (req, res) => {
+    Outfit.find({
+        isFavorite : true,
+        userID: new mongoose.Types.ObjectId(req.body.userID)
+    }).then((result) => {
+        console.debug("Retrieved all favorite outfits from the user");
+        res.send({result: result}).status(200);
+    }).catch((error) => {
+        console.error("Unable to retrieve all favorite outfits from the user", error);
+        res.send({message: "failure", reason: error}).status(500);
+    });
+});
 
 module.exports = router;
