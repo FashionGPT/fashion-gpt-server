09f2a91a8fe21b8fe50b6ffd776d46722bf4b544
Ameen Agbaria <ameenagbaria@lawn-128-61-71-50.lawn.gatech.edu>
2023-10-17 19:00:53 -0400

    creating a new branch for favorites

--- src/controller/Clothing.controller.js
+++ src/controller/Clothing.controller.js
@@ -4,6 +4,17 @@ const mongoose = require('mongoose');
 const Clothing = mongoose.model('Clothing');
 const Outfit = mongoose.model('Outfit');
 
+router.get('/clothing-feed', (req, res) => {
+    Outfit.find()
+    .sort({createdAt: -1})
+    .then((result) => {
+        console.debug("Retrieved all Clothing in timeline view");
+        res.send({result: result}).status(200);
+    }).catch((error) => {
+        console.error("Unable to retrieve all clothing objects", error);
+        res.send({message: "failure", reason: error}).status(500);
+    });
+});
 
 router.post('/get-favorites', (req, res) => {
     Outfit.find({
@@ -19,3 +30,4 @@ router.post('/get-favorites', (req, res) => {
 });
 
 module.exports = router;
+
