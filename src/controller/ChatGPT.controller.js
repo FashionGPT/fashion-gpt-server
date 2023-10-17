8c3671c55a19a76ccca897c23381f314eaee803f
b-dai <thebdaiman@gmail.com>
2023-10-17 19:42:15 -0400

    Finished working chatgpt response but error prone

--- src/controller/ChatGPT.controller.js
+++ src/controller/ChatGPT.controller.js
@@ -14,11 +14,12 @@ router.post('/create-outfit-from-chatgpt', (req, res) => {
     const requestFacts = req.body.prompt;
     openai.chat.completions.create({
         model: "gpt-3.5-turbo",
-        temperature: 0.6,
-        messages: [{"role": "user", "content": "Generate an outfit for me in JSON format with 'shirt' 'pants' and 'shoes' using the facts that " + requestFacts}],
+        temperature: 0.3,
+        // messages: [{"role": "user", "content": "Generate clothing that I should wear for the following scenario in JSON format where the 'shirt', 'pants', and 'shoes' are specified: " + requestFacts}],
+        messages: [{"role": "user", "content": "Give a JSON object for an outfit following this format: {'shirt':'x', 'pants':'y', 'shoes':'z'} for this scenario: " + requestFacts + ". Replace x, y, and z with respective pieces of clothing."}],
     }).then((gptResponse) => {
-        console.log(gptResponse.choices[0].message);
-        responseStr = gptResponse.choices[0].message;
+        responseStr = gptResponse.choices[0].message.content;
+        //console.debug("Chat GPT response", responseStr);
         res.send(JSON.parse(responseStr));
     }).catch((err) => {
         console.error("Unable to retrieve ChatGPT response", err);
