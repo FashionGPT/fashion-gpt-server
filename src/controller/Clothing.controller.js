6ae0364650fc390e58194a693c199617beb8c897
Sanskriti Gupta <sanskritigupta@lawn-128-61-8-114.lawn.gatech.edu>
2023-10-17 19:44:24 -0400

    removed message

--- src/controller/Clothing.controller.js
+++ src/controller/Clothing.controller.js
@@ -11,7 +11,7 @@ router.post('/get-outfit',(req, res) => {
     Outfit.find({
         userID: new mongoose.Types.ObjectId(req.body.userID)
     }).then((result) => {
-        res.send({message: "successfully generated an outfit for the user", result: result}).status(200);
+        res.send(result).status(200);
     }).catch((error) => {
         console.error("Unable to retrieve all the clothings", error);
         res.send({message: "failure", reason: error}).status(500);
