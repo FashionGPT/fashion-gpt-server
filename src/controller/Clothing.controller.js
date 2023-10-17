41dadaaec002c90935847ab5c168999dab113063
Sanskriti Gupta <99100219+sanskritiii04@users.noreply.github.com>
2023-10-17 19:52:41 -0400

    15 server side api for retrieving all clothings generated by a userid (#30)
    Co-authored-by: Sanskriti Gupta <sanskritigupta@lawn-128-61-6-67.lawn.gatech.edu>
    Co-authored-by: Sanskriti Gupta <sanskritigupta@lawn-128-61-8-114.lawn.gatech.edu>

--- src/controller/Clothing.controller.js
+++ src/controller/Clothing.controller.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Clothing = mongoose.model('Clothing');
 const Outfit = mongoose.model('Outfit');
 
 router.get('/clothing-feed', (req, res) => {
@@ -25,6 +24,18 @@ router.post('/get-favorites', (req, res) => {
         res.send({result: result}).status(200);
     }).catch((error) => {
         console.error("Unable to retrieve all favorite outfits from the user", error);
+        res.send({message: "failure", reason: error}).status(500);
+    });
+});
+
+// running query on clothing generation by user id
+router.post('/get-outfit',(req, res) => {
+    Outfit.find({
+        userID: new mongoose.Types.ObjectId(req.body.userID)
+    }).then((result) => {
+        res.send(result).status(200);
+    }).catch((error) => {
+        console.error("Unable to retrieve all the clothings", error);
         res.send({message: "failure", reason: error}).status(500);
     });
 });
