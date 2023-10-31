a903467cc98bef034dff94fcd9ef5347c6494b08
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2023-10-30 23:40:42 -0400

    22 server side function retrieve community posts (#32)

--- src/controller/Post.controller.js
+++ src/controller/Post.controller.js
@@ -1,28 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const Post = mongoose.model('Post');
+const mongoose = require("mongoose");
+const Post = mongoose.model("Post");
 
-
-router.post('/create', (req, res) => {
-    const postData = new Post();
-    postData.title = req.body.title;
-    postData.text = req.body.text;
-    postData.user = new mongoose.Types.ObjectId(req.body.user);
-    postData.outfit = new mongoose.Types.ObjectId(req.body.outfit);
-    postData.save().then((result) => {
-        console.debug("Saved post database object")
-        res.send({message: "successfully created new post database object", result: result}).status(200);
-    }).catch((error) => {
-        console.error("Unable to save post database object", error);
-        res.send({message: "failure", reason: error}).status(500);
+router.post("/create", (req, res) => {
+  const postData = new Post();
+  postData.title = req.body.title;
+  postData.text = req.body.text;
+  postData.user = new mongoose.Types.ObjectId(req.body.user);
+  postData.outfit = new mongoose.Types.ObjectId(req.body.outfit);
+  postData
+    .save()
+    .then((result) => {
+      console.debug("Saved post database object");
+      res
+        .send({
+          message: "successfully created new post database object",
+          result: result,
+        })
+        .status(200);
+    })
+    .catch((error) => {
+      console.error("Unable to save post database object", error);
+      res.send({ message: "failure", reason: error }).status(500);
     });
 });
 
-
-
-
+router.get("/community", (req, res) => {
+  Post.find()
+    .then((result) => {
+      res.send({ result: result }).status(200);
+    })
+    .catch((error) => {
+      console.error("Unable to fetch post database object", error);
+      res.send({ message: "failure", reason: error }).status(500);
+    });
+});
 
 module.exports = router;
-
-
